29ba76ff6373bc3a6ee09ec194028bd9472a7134
MooKorea <69235304+MooKorea@users.noreply.github.com>
2023-04-13 21:03:05 -0500

    Gun aiming initial implementation

--- Assets/Scripts/Raycast.cs
+++ Assets/Scripts/Raycast.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class Raycast : MonoBehaviour
 {
+    private Vector3 worldPosition;
+    [SerializeField] private Vector3 offset;
+    [SerializeField] private GameObject target;
+    
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,15 @@ public class Raycast : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 mousePos = Input.mousePosition;
+        mousePos.z = Camera.main.nearClipPlane;
+        worldPosition = Camera.main.ScreenToWorldPoint(mousePos);
+        transform.position = new Vector3(worldPosition.x - offset.x, worldPosition.y - offset.y, worldPosition.z - offset.z);
+
+        Ray ray = new Ray(transform.position, transform.forward);
+        RaycastHit hitData;
+        Physics.Raycast(ray, out hitData);
+        target.transform.position = hitData.point;
+        Debug.Log(hitData.point);
     }
 }
