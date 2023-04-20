efca71c429cdcac93fa0e27310cce74199715214
MooKorea <69235304+MooKorea@users.noreply.github.com>
2023-04-20 18:46:45 -0500

    aim thingy and player movement support
    partially

--- Assets/Scripts/GunMover.cs
+++ Assets/Scripts/GunMover.cs
@@ -8,20 +8,21 @@ public class GunMover : MonoBehaviour {
     [SerializeField] private Transform MainCamera;
     [SerializeField] private Transform reference;
     [SerializeField] private Transform viewportReference;
+    [SerializeField] private Transform playerGunPos;
     [SerializeField] private Quaternion rotationOffset;
-    [SerializeField] private float ZOffset;
     [SerializeField] private float YOffset;
     [SerializeField] private float moveSensitivity = 1f;
 
 
     void Update() {
         Vector3 pos = MainCamera.TransformPoint(ARCamera.InverseTransformPoint(reference.position));
+        Vector3 gunPos = playerGunPos.transform.position;
 
-        viewportReference.position = new Vector3(pos.x, pos.y + YOffset, ZOffset);
+        viewportReference.position = new Vector3(pos.x, pos.y + YOffset, gunPos.z);
         Vector3 posViewport = Camera.main.WorldToViewportPoint(viewportReference.position) + new Vector3(-0.5f, -0.5f, 0);
         posViewport = posViewport * moveSensitivity;
 
-        transform.position = new Vector3(pos.x + posViewport.x, pos.y + posViewport.y + YOffset, ZOffset);
+        transform.position = new Vector3(pos.x + posViewport.x, pos.y + posViewport.y + YOffset, gunPos.z);
 
         transform.rotation = MainCamera.rotation * Quaternion.Euler(0, 0, 180) * Quaternion.Inverse(ARCamera.rotation) * reference.rotation * rotationOffset;
     }
--- Assets/Scripts/Player/Shoot.cs
+++ Assets/Scripts/Player/Shoot.cs
@@ -8,16 +8,21 @@ public class Shoot : MonoBehaviour
     [SerializeField] private GameObject spawner;
     [SerializeField] private GameObject bullet;
     [SerializeField] private float fireRate = 10f;
+    [SerializeField] private GameObject crossHair;
+    [SerializeField] private LayerMask bulletLayerMask;
 
+    [SerializeField] private GameObject recoilObject;
     [SerializeField] private float recoilStrength = 5f;
     
     private float _currentFireCountdown = 1f;
     private bool isShooting;
+    private Vector3 initialZPos;
 
     // Start is called before the first frame update
     void Start()
     {
         isShooting = true;
+        initialZPos = transform.localPosition;
     }
 
     // Update is called once per frame
@@ -25,8 +30,17 @@ public class Shoot : MonoBehaviour
     {
         if (isShooting)
         {
+            FireRay();
             ShootBullet();
         }
+    }
+
+    void FireRay()
+    {
+        Ray ray = new Ray(spawner.transform.position, spawner.transform.forward);
+        RaycastHit hitData;
+        Physics.Raycast(ray, out hitData, 1000f, ~bulletLayerMask);
+        crossHair.transform.position = hitData.point;
     }
 
     private void ShootBullet()
@@ -42,8 +56,9 @@ public class Shoot : MonoBehaviour
 
     private void Recoil()
     {
-        Vector3 pos = transform.position;
-        transform.DOMove(new Vector3(pos.x, pos.y, pos.z - (0.01f * recoilStrength)), 0.02f);
+        // transform.DOMove(new Vector3(pos.x, pos.y, pos.z - (0.01f * recoilStrength)), 0.02f);
+        recoilObject.transform.localPosition = new Vector3(0, 0, 0); 
+        recoilObject.transform.DOLocalMoveZ(0f - (0.1f * recoilStrength), 0.01f);
     }
 
 
